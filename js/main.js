90da95decae772c98a95eff0c5acc1c838bd5898
rajparakh92 <37536036+rajparakh92@users.noreply.github.com>
2020-07-19 13:23:40 +0530

    design and logo

--- js/main.js
+++ js/main.js
@@ -137,17 +137,17 @@
     { id: "1", name: "Dulhan Lehenga", image: "Dulhan Lehnga - 6499", price: "6499", type: "new-arrival" },
     { id: "2", name: "Net Party wear Lehenga", image: "Net Party wear Lehnga - 3999", price: "3999", type: "festive" },
     { id: "3", name: "Party wear Lehenga", image: "Party wear Lehnga - 6999", price: "6999", type: "new-arrival" },
-    { id: "4", name: "Weightless 1", image: "Weightless - 399", price: "399", type: "festive" },
-    { id: "5", name: "Weightless 2", image: "Weightless 1 - 399", price: "399", type: "offers" },
-    { id: "6", name: "Weightless 3", image: "Weightless 2 - 399", price: "399", type: "new-arrival" },
-    { id: "7", name: "Weightless 4", image: "Weightless 3 - 399", price: "399", type: "offers" }
+    { id: "4", name: "Weightless Chex", image: "Weightless - 399", price: "399", type: "festive" },
+    { id: "5", name: "Weightless Green", image: "Weightless 1 - 399", price: "399", type: "offers" },
+    { id: "6", name: "Weightless Marron", image: "Weightless 2 - 399", price: "399", type: "new-arrival" },
+    { id: "7", name: "Weightless Red", image: "Weightless 3 - 399", price: "399", type: "offers" }
   ]
 
   for (let i = 0; i < items.length; i++) {
     $("#items").append('<div class="col-lg-2 col-6 portfolio-item ' + items[i].type +
       '" data-wow-delay="0.1s"> <div class="portfolio-wrap"> <img src="img/portfolio/' + items[i].image
       + '.jpg" class="img-fluid" alt=""> <div class="portfolio-info"><div> <a href="img/portfolio/' +
-      items[i].image + '.jpg" data-lightbox="portfolio" data-title="App 1" class="link-preview" title="Preview"><i class="ion ion-eye"></i>' +
+      items[i].image + '.jpg" data-lightbox="portfolio" data-title="' + items[i].name + '" class="link-preview" title="Preview"><i class="ion ion-eye"></i>' +
       '</a></div></div></div><p class="item-label text-center">' +
       items[i].name + ' <br/><b>&#x20b9;' + items[i].price + ' </b><br/>' +
       '<button name="' + items[i].name + '" class="btn btn-primary buy-btn">Buy</button></p></div>');
