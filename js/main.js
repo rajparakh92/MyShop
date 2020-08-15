7085bace2521bafdd01c14dd2a76c1fd573c7cb1
rajparakh92 <37536036+rajparakh92@users.noreply.github.com>
2020-08-15 13:12:05 +0530

    New Products

--- js/main.js
+++ js/main.js
@@ -134,24 +134,38 @@
   $('#mob').text(mobileNumber);
 
   var items = [
-    { id: "1", name: "Dulhan Lehenga", image: "Dulhan Lehnga - 6499.png", price: "5999", type: "offers" },
-    { id: "2", name: "Net Party wear Lehenga", image: "Net Party wear Lehnga - 3999.png", price: "3999", type: "festive" },
-    { id: "3", name: "Party wear Lehenga", image: "Party Wear Lehenga - 6499.png", price: "6499", type: "festive" },
-    { id: "4", name: "Weightless Chex", image: "Weightless - 399.png", price: "449", type: "festive" },
-    { id: "5", name: "Weightless Green", image: "Weightless 1 - 399.png", price: "449", type: "offers" },
-    { id: "6", name: "Weightless Marron", image: "Weightless 2 - 399.png", price: "449", type: "festive" },
-    { id: "7", name: "Weightless Red", image: "Weightless 3 - 399.png", price: "449", type: "offers" },
-    { id: "8", name: "Weightless Diamod", image: "Weightless - Diamod - 249.png", price: "299", type: "offers" },
-    { id: "9", name: "Weightless Titli", image: "Weightless Titli - 299.png", price: "349", type: "new-arrival" },
-    { id: "10", name: "Bengali Buta", image: "Bengali Buta - 299.png", price: "349", type: "new-arrival" },
-    { id: "11", name: "Weightless Sarika", image: "Weightless Sarika 249.png", price: "299", type: "new-arrival" },
-    { id: "12", name: "Cotton Print", image: "Cotton Print - 349.png", price: "399", type: "new-arrival" },
-    { id: "13", name: "Cotton Print New", image: "Cotton Print 1 - 349.png", price: "399", type: "new-arrival" },
-    { id: "14", name: "Weightless with Less", image: "Weightless with Less - 249.png", price: "349", type: "new-arrival" },
-    { id: "15", name: "Foam Work", image: "Foam Work - 499.png", price: "549", type: "new-arrival" },
-    { id: "16", name: "Diamond Work", image: "Diamond - 499.png", price: "549", type: "new-arrival" },
-    { id: "17", name: "Weightless Work", image: "Weightless Work - 449.png", price: "499", type: "new-arrival" },
-    { id: "18", name: "Kajal Saree", image: "Kajal Saree - 349.png", price: "399", type: "new-arrival" }
+    { id: "1", name: "Dulhan Lehenga", image: "Dulhan Lehnga - 6499.png", price: "5999", type: "Lehengas" },
+    { id: "2", name: "Net Party wear Lehenga", image: "Net Party wear Lehnga - 3999.png", price: "3999", type: "Lehengas" },
+    { id: "3", name: "Party wear Lehenga", image: "Party Wear Lehenga - 6499.png", price: "6499", type: "Lehengas" },
+    { id: "4", name: "Weightless Chex Saree", image: "Weightless - 399.png", price: "449", type: "Sarees" },
+    { id: "5", name: "Weightless Green Saree", image: "Weightless 1 - 399.png", price: "449", type: "Sarees" },
+    { id: "6", name: "Weightless Marron Saree", image: "Weightless 2 - 399.png", price: "449", type: "Sarees" },
+    { id: "7", name: "Weightless Red Saree", image: "Weightless 3 - 399.png", price: "449", type: "Sarees" },
+
+    { id: "8", name: "Ashwika Net Lehenga", image: "Ashwika_Net_Lehenka_-_3999.png", price: "3999", type: "Lehengas" },
+    { id: "9", name: "Laddu Brown Saree", image: "Laddu_Brown_-_299.png", price: "299", type: "Sarees" },
+    { id: "10", name: "Laddu Purple Saree", image: "Laddu_Purple_-_299.png", price: "299", type: "Sarees" },
+    { id: "11", name: "Lehenga Saree", image: "Lehenga_Saree_-_4999.png", price: "4999", type: "Lehengas" },
+    { id: "12", name: "Madhuri Dark Blue Saree", image: "Madhuri_Dark_Blue-_499.png", price: "499", type: "Sarees" },
+    { id: "13", name: "Mristha Lehenga", image: "Mristha_Lehenga_-_6999.png", price: "6999", type: "Lehengas" },
+    { id: "14", name: "Shreedevi Gold 60 gm", image: "Shreedevi_Gold_60_gm_-_499.png", price: "499", type: "Sarees" },
+    { id: "15", name: "Chiffon Ginni Gold Saree", image: "Sifon_Ginni_Gold_-_599.png", price: "599", type: "Sarees" },
+    { id: "16", name: "Sonakhi Blue 60 gm Saree", image: "Sonakhi_Blue_60_gm_-_599.png", price: "599", type: "Sarees" },
+    { id: "17", name: "Weightless Dark Mehendi", image: "Weightless_Dark_Mehendi_60_gm_-_399.png", price: "399", type: "Sarees" },
+    { id: "18", name: "Weightless Padding Saree", image: "Weightless_Padding_-_299.png", price: "299", type: "Sarees" },
+
+    { id: "19", name: "Weightless Diamond", image: "Weightless - Diamod - 249.png", price: "299", type: "Sarees" },
+    { id: "20", name: "Weightless Titli Saree", image: "Weightless Titli - 299.png", price: "349", type: "Sarees" },
+    { id: "21", name: "Bengali Buta Saree", image: "Bengali Buta - 299.png", price: "349", type: "Sarees" },
+    { id: "22", name: "Weightless Sarika Saree", image: "Weightless Sarika 249.png", price: "299", type: "Sarees" },
+    { id: "23", name: "Cotton Print Saree", image: "Cotton Print - 349.png", price: "399", type: "Sarees" },
+    { id: "24", name: "Cotton Print New Saree", image: "Cotton Print 1 - 349.png", price: "399", type: "Sarees" },
+    { id: "25", name: "Weightless with Less", image: "Weightless with Less - 249.png", price: "349", type: "Sarees" },
+    { id: "26", name: "Foam Work Saree", image: "Foam Work - 499.png", price: "549", type: "Sarees" },
+    { id: "27", name: "Diamond Work Saree", image: "Diamond - 499.png", price: "549", type: "Sarees" },
+    { id: "28", name: "Weightless Work Saree", image: "Weightless Work - 449.png", price: "499", type: "Sarees" },
+    { id: "29", name: "Kajal Saree", image: "Kajal Saree - 349.png", price: "399", type: "Sarees" },
+    { id: "30", name: "Weightless Dark Blue", image: "Weightless Dark Blue - 499.png", price: "499", type: "Sarees" }
   ]
 
   for (let i = 0; i < items.length; i++) {
